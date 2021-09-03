9d6ad9e6842af5b38965427789896d8966db72c0
OZAN\ozan <ozan.kahveci15@gmail.com>
2021-09-03 12:47:57 +0300

    Cont1 sahne ekledi

---
+++ Assets/Scenes/ContScript1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContScript1 : MonoBehaviour
+{
+    private Transform t1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t1 = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
